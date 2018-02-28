0239bca1d6b5ec85b02edf8adb1475c9597a9934
harrisonforch <forch@example.edu>
2018-02-28 01:49:16 +0000

    changed sound sensor ADC port and changed threshold

--- testSuite.py
+++ testSuite.py
@@ -28,22 +28,22 @@ while True:
 			print("light: %d" % (value))
 		else:
 			print("dark: %d" % (value))
-		
+
 		time.sleep(.1)
-	
+
 	print("Blinking light 4 times at 200ms intervals")
 	for x in range(0,4):
 		GPIO.output(11, GPIO.HIGH)
 		time.sleep(0.2)
 		GPIO.output(11, GPIO.LOW)
 		time.sleep(0.2)
-	
+
 	print("reading sound sensor, LED on if ADC reads above 500")
 	for y in range(0,50):
-		value = mcp.read_adc(0)
-		if value> 500:
+		value = mcp.read_adc(1)
+		if value> 650:
 			GPIO.output(11, GPIO.HIGH)
-		
+
 		print(value)
 		time.sleep(.1)
 		GPIO.output(11,GPIO.LOW)
